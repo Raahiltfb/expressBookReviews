01b6aae6403bbc60ca73b9b6161d6bdbb39102b8
Raahiltfb <raahil.mehta003@gmail.com>
2024-10-11 11:08:23 +0530

    Update auth_users.js
    New changes to the tasks (1-7)

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -14,16 +14,44 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+ const username = req.params.username;
+  let books = books[username];
+
+  if (books) {
+    let review = req.body.review;
+
+    if (review) {
+      books["review"] = review;
+    }
+
+    books[username] = books;
+    res.send('Book with the 
+    
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
